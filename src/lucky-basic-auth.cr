b61c79c676bfa30c21adcd919fb01b448e1cbb22
Russ Smith <russ@bashme.org>
2018-12-11 12:00:57 -0800

    Changing this so it's called as a macro that handles the heavy lifting.

--- src/lucky-basic-auth.cr
+++ src/lucky-basic-auth.cr
@@ -1,34 +1,59 @@
 require "base64"
 
 module Lucky::BasicAuthPipe
-  VERSION = "0.1.0"
-
+  VERSION               = "0.1.0"
   BASIC                 = "Basic"
   AUTH                  = "Authorization"
   AUTH_MESSAGE          = "Could not verify your access level.\nYou must login to continue"
   HEADER_LOGIN_REQUIRED = %{Basic realm="Login Required"}
 
-  private def authenticate_login
-    show_login = true
-    if value = @context.request.headers[AUTH]?
-      if value.size > 0 && value.starts_with?(BASIC)
-        if authorized?(value)
-          show_login = false
+  macro basic_auth
+    def authorize
+      show_login = true
+      if value = @context.request.headers[AUTH]?
+        if value.size > 0 && value.starts_with?(BASIC)
+          username, password = Base64.decode_string(value[BASIC.size + 1..-1]).split(":")
+          if username == ENV["AUTH_USERNAME"] && ENV["AUTH_PASSWORD"]
+            show_login = false
+          end
         end
       end
+
+      if show_login
+        headers = HTTP::Headers.new
+        @context.response.status_code = 401
+        @context.response.headers["WWW-Authenticate"] = HEADER_LOGIN_REQUIRED
+        text AUTH_MESSAGE
+      else
+        continue
+      end
     end
-    if show_login
-      headers = HTTP::Headers.new
-      @context.response.status_code = 401
-      @context.response.headers["WWW-Authenticate"] = HEADER_LOGIN_REQUIRED
-      text AUTH_MESSAGE
-    else
-      continue
-    end
+
+    before authorize
   end
 
-  private def authorized?(value)
-    username, password = Base64.decode_string(value[BASIC.size + 1..-1]).split(":")
-    username == ENV["AUTH_USERNAME"] && password == ENV["AUTH_PASSWORD"]
+  macro basic_auth(block)
+    def authorize
+      show_login = true
+      if value = @context.request.headers[AUTH]?
+        if value.size > 0 && value.starts_with?(BASIC)
+          username, password = Base64.decode_string(value[BASIC.size + 1..-1]).split(":")
+          if {{block.body}}
+            show_login = false
+          end
+        end
+      end
+
+      if show_login
+        headers = HTTP::Headers.new
+        @context.response.status_code = 401
+        @context.response.headers["WWW-Authenticate"] = HEADER_LOGIN_REQUIRED
+        text AUTH_MESSAGE
+      else
+        continue
+      end
+    end
+
+    before authorize
   end
 end

837b057d1abc9afec0ad4fa31ed4fcf364c14c3b
Jeremy Woertink <jeremywoertink@gmail.com>
2021-04-19 11:36:38 -0700

    bump versions for new release (#3)

--- src/lucky-basic-auth.cr
+++ src/lucky-basic-auth.cr
@@ -2,7 +2,7 @@ require "base64"
 require "crypto/subtle"
 
 module Lucky::BasicAuthPipe
-  VERSION               = "0.2.0"
+  VERSION               = "0.2.1"
   BASIC                 = "Basic"
   AUTH                  = "Authorization"
   AUTH_MESSAGE          = "Could not verify your access level.\nYou must login to continue"
